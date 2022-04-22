d33b96cfe14bd80e11169dd924b8958b059c989a
Shakirah Cooper <67792212+ArchaePi@users.noreply.github.com>
2022-04-22 13:19:20 -0400

    Implemented Text to Speech functionality.

--- app/src/main/java/edu/temple/audibleonmobileapplication/DisplayPageFragment.kt
+++ app/src/main/java/edu/temple/audibleonmobileapplication/DisplayPageFragment.kt
@@ -1,14 +1,21 @@
 package edu.temple.audibleonmobileapplication
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import java.util.*
 
 
-class DisplayPageFragment : Fragment() {
+class DisplayPageFragment : Fragment(), TextToSpeech.OnInitListener {
+    private var tts: TextToSpeech? = null
+    private var speakBtn: Button? = null
+    private var editText : EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,14 +29,24 @@ class DisplayPageFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_display_page, container, false)
         val back_button = root.findViewById<Button>(R.id.back_button)
+        speakBtn = root.findViewById(R.id.audio_button)
+        editText = root.findViewById(R.id.translated_text)
+        //tts = TextToSpeech(activity?.applicationContext, this)//
+
+        speakBtn!!.isEnabled = false
+        tts = TextToSpeech(activity?.applicationContext, this)
 
         back_button.setOnClickListener {
-            val fragment = TranslationPageFragment()
+            val fragment = HomePageFragment()
             val fragmentManager = activity?.supportFragmentManager
             val fragmentTransaction = fragmentManager?.beginTransaction()
             fragmentTransaction?.replace(R.id.fragmentContainerView, fragment)
             fragmentTransaction?.addToBackStack(null)
             fragmentTransaction?.commit()
+        }
+
+        speakBtn!!.setOnClickListener {
+            speakOut()
         }
         // Inflate the layout for this fragment
         return root
@@ -42,5 +59,32 @@ class DisplayPageFragment : Fragment() {
                 arguments = Bundle().apply {
                 }
             }
+    }
+
+    override fun onInit(p0: Int) {
+        if (p0 == TextToSpeech.SUCCESS) {
+            val result = tts!!.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS","The Language not supported!")
+            } else {
+                speakBtn!!.isEnabled = true
+            }
+        }
+    }
+
+    private fun speakOut() {
+        val text = editText!!.text.toString()
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null,"")
+    }
+
+    override fun onDestroy() {
+        // Shutdown TTS when
+        // activity is destroyed
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
     }
 }
